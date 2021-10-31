5dc66fd3ca5b3815d203d017d67440bb3ad25ef8
tomfran <18344761+tomfran@users.noreply.github.com>
2021-10-31 18:18:14 +0100

    Added vectorizers and tokenizers

--- src/tokenizers.py
+++ src/tokenizers.py
@@ -1,7 +1,33 @@
 from abc import ABC, abstractmethod
+import spacy
+import nltk
 
 class Tokenizer(ABC): 
     
     @abstractmethod
     def tokenize(self, text):
         pass
+    
+class SpacyTokenizer(Tokenizer):
+
+    def __init__(self):
+        self.nlp = spacy.load("en_core_web_sm")
+
+    def tokenize(self, text):
+        return [token.lemma_ for token in self.nlp(text) if
+                token.pos_ in ['NOUN', 'PROPN', 'VERB', 'ADJ', 'ADV']]
+
+class NLTKTokenizer(Tokenizer):
+
+    def tokenize(self, text):
+        return nltk.word_tokenize(text)
+        
+if __name__ == "__main__": 
+    
+    s = "An Apple 'fell', \tfrom the\n tree"
+    t = SpacyTokenizer()
+    print(t.tokenize(s))
+    
+    t = NLTKTokenizer()
+    print(t.tokenize(s))
+    
--- src/vectorizers.py
+++ src/vectorizers.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from .tokenizers import Tokenizer
+from tokenizers import *
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 class Vectorizer(ABC):
 
@@ -14,3 +15,26 @@ class Vectorizer(ABC):
     @abstractmethod
     def vec(self, document):
         pass
+    
+class TfIdfVectors(Vectorizer):
+
+    def __init__(self, documents, tokenizer):
+        super().__init__(documents, tokenizer)
+        self.vectorizer = TfidfVectorizer(tokenizer=self.tokenizer.tokenize)
+
+    def vectors(self):
+        return self.vectorizer.fit_transform(self.documents)
+
+    def vec(self, document):
+        return self.vectorizer.transform([document])
+    
+if __name__ == "__main__":
+    doc = ["An apple fell from the tree", 
+           "apple apple apple apple apple was founded in 4543 inside a tree"]
+    
+    t = SpacyTokenizer()
+    v = TfIdfVectors(doc, t)
+    
+    from pprint import pprint
+    print(v.vectors())
+    print(v.vectorizer.vocabulary_)
