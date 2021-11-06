639e0e3d0dcad61478cdc8b09da79902a8af3316
tomfran <18344761+tomfran@users.noreply.github.com>
2021-11-06 16:27:34 +0100

    Added pipe to tokenizer and created a new batch tokenizer

--- src/tokenizers.py
+++ src/tokenizers.py
@@ -1,13 +1,17 @@
 from abc import ABC, abstractmethod
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+from functools import reduce
+
 import spacy
 import nltk
-from functools import reduce
 
 from .abstract_classes import Tokenizer
 
+
 class DocumentTokenizer(Tokenizer):
 
-    def __init__(self):
+    def __init__(self, n_workers=4):
         self.nlp = spacy.load("en_core_web_sm")
 
     def tokenize_sentences(self, text):        
@@ -17,14 +21,29 @@ class DocumentTokenizer(Tokenizer):
         return [token.lemma_ for token in self.nlp(text) if
                 token.pos_ in ['NOUN', 'PROPN', 'VERB', 'ADJ', 'ADV']]
         
-    def tokenize(self, text, get_sentences=False):
+    def tokenize(self, text, get_sentences=True):
         if get_sentences:
-            sentences = self.tokenize_sentences(text)
-            return reduce(lambda a, b: a + b,
-                          [self.spacy_tokenization(sent) for sent in sentences])
+            sentences = self.tokenize_sentences(text)            
+            tokens = []
+            for sent in self.nlp.pipe(sentences, n_process=-1):
+                tokens += [token.lemma_ for token in sent 
+                           if token.pos_ in ['NOUN', 'PROPN', 'VERB', 'ADJ', 'ADV']]
+            return tokens
         else:
             return self.spacy_tokenization(text)
         
+class BatchTokenizer(Tokenizer):
+    
+    def __init__(self, n_workers=4):
+        self.nlp = spacy.load("en_core_web_sm")
+    
+    def tokenize(self, documents):
+        tokens = []
+        for doc in self.nlp.pipe(documents, n_process=-1):
+            tokens.append([token.lemma_ for token in doc 
+                        if token.pos_ in ['NOUN', 'PROPN', 'VERB', 'ADJ', 'ADV']])
+        return tokens
+    
 if __name__ == "__main__":
     
     from pprint import pprint
@@ -36,4 +55,7 @@ if __name__ == "__main__":
     
     print(timeit.timeit('t.tokenize(text)',
                         setup='from __main__ import t, text', 
-                        number=3))
+                        number=1))
+    print(timeit.timeit('t.tokenize(text, False)',
+                        setup='from __main__ import t, text', 
+                        number=1))
